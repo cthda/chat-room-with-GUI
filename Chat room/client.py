580e5d7568865412ea16efb2b4522724b4ed6d28
cthda <100998876+cthda@users.noreply.github.com>
2022-05-10 21:22:33 +0300

    Add files via upload

---
+++ Chat room/client.py
@@ -0,0 +1,574 @@
+import socket
+import tkinter as tk
+from tkinter import messagebox as msgbox
+import threading
+import tkinter.scrolledtext as scrolledtxt
+
+
+
+
+class MainWindow(tk.Tk):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.wm_geometry("700x800")
+
+        self.wm_minsize(670,740)
+
+        self.wm_maxsize(1920,1080)
+
+        self.wm_resizable(True,True)
+
+        self.wm_title("Log in")
+
+        self.username_input_entry = tk.StringVar()
+
+        self.password_input_entry = tk.StringVar()
+
+    def set_and_place_username_entry(self):
+
+        def placeholder_for_username_field(event):
+
+            self.username_entry_object["state"] = "normal"
+
+            self.username_entry_object.delete(0,"end")
+
+            self.username_entry_object.unbind("<Button-1>",self.placeholder_username_id)
+
+        self.username_entry_object = tk.Entry(
+        self,
+        textvariable=self.username_input_entry,
+        justify="left",
+        bg="white",
+        fg="black",
+        font=(100)
+        )
+
+        self.username_entry_object.place_configure(relx=0.4,rely=0.3,relheight=0.04,relwidth=0.25)
+
+        self.username_entry_object.insert(0,"Username")
+
+        self.username_entry_object.configure(state="disabled",disabledbackground="white")
+
+        self.placeholder_username_id = self.username_entry_object.bind("<Button-1>",placeholder_for_username_field)
+
+    def set_and_place_info_label(self):
+
+        def change_color_on_motion(event):
+
+            self.info_label_object["fg"] = "orange"
+
+        def move_out_mouse_from_area_of_label(event):
+
+            self.info_label_object["fg"] = "black"
+
+        def click_on_label(event):
+
+            self.info_label_object.place_configure(relwidth=0.25)
+
+            self.info_label_object["text"] = "The Username field will be shown\nas the nickname in the chat room"
+
+            self.info_label_object.bind("<Button-1>",click_on_label_again)
+
+        def click_on_label_again(event):
+
+            self.info_label_object.place_configure(relwidth=0.05)
+
+            self.info_label_object["text"] = "Info"
+
+            self.info_label_object.bind("<Button-1>",click_on_label)
+
+        self.info_label_object = tk.Label(
+        self,
+        bg="white",
+        fg="black",
+        text="Info",
+        cursor="hand2"
+        )
+
+        self.info_label_object.place_configure(relx=0.4,rely=0.2656,relheight=0.032,relwidth=0.05)
+
+        self.info_label_object.bind("<Motion>",change_color_on_motion)
+
+        self.info_label_object.bind("<Leave>",move_out_mouse_from_area_of_label)
+
+        self.info_label_object.bind("<Button-1>",click_on_label)
+
+    def set_and_place_show_label(self):
+
+        def change_color_on_motion(event):
+
+            self.show_label_object["fg"] = "red"
+
+        def move_out_mouse_from_area_of_label(event):
+
+            self.show_label_object["fg"] = "black"
+
+        def click_on_label(event):
+
+            self.password_entry_object["show"] = ""
+
+            self.show_label_object["text"] = "Hide"
+
+            self.show_label_object.bind("<Button-1>",click_on_label_again)
+
+        def click_on_label_again(event):
+
+            self.password_entry_object["show"] = "*"
+
+            self.show_label_object["text"] = "Show"
+
+            self.show_label_object.bind("<Button-1>",click_on_label)
+
+        self.show_label_object = tk.Label(
+        self,
+        bg="white",
+        fg="black",
+        text="Show",
+        cursor="hand2",
+        )
+
+        self.show_label_object.place_configure(relx=0.6,rely=0.40,relheight=0.032,relwidth=0.05)
+
+        self.show_label_object.bind("<Motion>",change_color_on_motion)
+
+        self.show_label_object.bind("<Leave>",move_out_mouse_from_area_of_label)
+
+        self.show_label_object.bind("<Button-1>",click_on_label)
+
+    def set_and_place_password_entry(self):
+
+        def placeholder_for_password_field(event):
+
+            self.password_entry_object["state"] = "normal"
+
+            self.password_entry_object.delete(0,"end")
+
+            self.password_entry_object.unbind("<Button-1>",self.placeholder_password_id)
+
+            self.password_entry_object["show"] = "*"
+
+        self.password_entry_object = tk.Entry(
+        self,
+        textvariable=self.password_input_entry,
+        justify="left",
+        bg="white",
+        fg="black",
+        font=(100)
+        )
+
+        self.password_entry_object.place_configure(relx=0.4,rely=0.36,relheight=0.04,relwidth=0.25)
+
+        self.password_entry_object.insert(0,"Password")
+
+        self.password_entry_object.configure(state="disabled",disabledbackground="white")
+
+        self.placeholder_password_id = self.password_entry_object.bind("<Button-1>",placeholder_for_password_field)
+
+    def set_and_place_enter_button(self):
+
+        self.enter_button_object = tk.Button(
+        self,
+        text="Enter",
+        command=self.__open_the_chat_window,
+        bg="white",
+        fg="black",
+        state="normal",
+        activebackground="white",
+        activeforeground="black",
+        disabledforeground="black",
+        cursor="hand2",
+        font=(100)
+        )
+
+        self.enter_button_object.place_configure(relx=0.46,rely=0.47,relheight=0.05,relwidth=0.12)
+
+    def __open_the_chat_window(self):  #This one actually starts the chat room
+
+        try:
+
+            self.client_connection = SocketConnection(socket.AF_INET,socket.SOCK_STREAM,"127.0.0.1",11111)
+
+        except:
+
+            msgbox.showerror("Error","Connection Error")
+
+        else:
+
+            self.username = self.username_input_entry.get()
+
+            self.password = self.password_input_entry.get()
+
+            try:
+
+                self.client_connection.send(f"****UsErNamEToLOgin---PasSwORdToLOgin__?? {self.username} {self.password}".encode("utf-8"))  #sqlite has no server
+
+            except:
+
+                self.client_connection.close()
+
+                msgbox.showerror("Error","Connection Error")
+
+            else:
+
+                try:
+
+                    self.client_connection.settimeout(5.5)
+
+                    self.message = self.client_connection.recv(1024).decode("utf-8")  #May raise error because of some cases
+
+                    self.client_connection.settimeout(None)
+
+                    if self.message == "FALSE":   #Wrong username or password
+
+                        self.client_connection.close()
+
+                        msgbox.showerror("Error","Wrong username or password")
+
+                    elif self.message == "TRUE":     #If no problem for username and password while entering
+
+                        self.chat_window = ChatWindow(self)  
+
+                        self.user_input = self.chat_window.create_input_of_users_and_return_object()
+
+                        self.send_button_object = self.chat_window.create_send_button_and_return_object()
+
+                        self.text_screen = self.chat_window.create_text_widget_to_display_messages_and_return_object()
+
+                        self.thread_to_get_message = threading.Thread(target=self.client_connection.recv_messages,args=(self.text_screen,),daemon=True)
+
+                        self.thread_to_get_message.start()
+
+                        self.send_button_object["command"] = lambda:self.client_connection.send_messages(self.user_input,self.username)
+
+                except socket.timeout:
+
+                    self.client_connection.close()
+
+                    msgbox.showerror("Error","Connection timeout")
+
+                except:
+
+                    self.client_connection.close()
+
+                    msgbox.showerror("Error","Connection has been shutted")
+
+                else:
+
+                    pass
+
+    def set_and_place_signup_button(self):
+
+        self.signup_button_object = tk.Button(
+        self,
+        text="Sign up",
+        command=self.__after_click_signup_button,
+        bg="white",
+        fg="black",
+        state="normal",
+        activebackground="white",
+        activeforeground="black",
+        disabledforeground="black",
+        cursor="hand2",
+        font=(100)
+        )
+
+        self.signup_button_object.place_configure(relx=0.83,rely=0.09,relheight=0.05,relwidth=0.12)
+
+    def __after_click_signup_button(self):
+
+        def placeholder_for_username_field(event):
+
+            self.username_entry_object["state"] = "normal"
+
+            self.username_entry_object.delete(0,"end")
+
+            self.username_entry_object.unbind("<Button-1>",self.placeholder_username_id)
+
+        self.username_entry_object["state"] = "normal"
+
+        self.username_entry_object.delete(0,"end")
+
+        self.username_entry_object.insert(0,"Username")
+
+        self.username_entry_object.configure(state="disabled",disabledbackground="white")
+
+        self.placeholder_username_id = self.username_entry_object.bind("<Button-1>",placeholder_for_username_field)
+
+        def placeholder_for_password_field(event):
+
+            self.password_entry_object["state"] = "normal"
+
+            self.password_entry_object.delete(0,"end")
+
+            self.password_entry_object.unbind("<Button-1>",self.placeholder_password_id)
+
+            self.password_entry_object["show"] = "*"
+
+        self.password_entry_object.configure(show="",state="normal")
+
+        self.password_entry_object.delete(0,"end")
+
+        self.password_entry_object.insert(0,"Password")
+
+        self.password_entry_object.configure(state="disabled",disabledbackground="white")
+
+        self.placeholder_password_id = self.password_entry_object.bind("<Button-1>",placeholder_for_password_field)
+
+        def sign_up_person():  #This one actually starts to sign up the person to the chat room
+
+            try:
+
+                client_connection_to_signup = SocketConnection(socket.AF_INET,socket.SOCK_STREAM,"127.0.0.1",11111)
+
+            except:
+
+                msgbox.showerror("Error","Connection Error")
+
+            else:
+
+                username_to_signup = self.username_input_entry.get()
+
+                password_to_signup = self.password_input_entry.get()
+
+                if (len(username_to_signup) >= 4 and len(password_to_signup) >= 4) and (username_to_signup != "Username" and password_to_signup != "Password"):
+
+                    try:
+
+                        client_connection_to_signup.send(f"****UsErNamE---PasSwORd__?? {username_to_signup} {password_to_signup}".encode("utf-8")) #sqlite has no server
+
+                    except:
+
+                        client_connection_to_signup.close()
+
+                        msgbox.showerror("Error","Connection Error")
+
+                    else:
+
+                        try:
+
+                            client_connection_to_signup.settimeout(5.5)
+
+                            signup_message = client_connection_to_signup.recv(1024).decode("utf-8")  #May raise error because of some cases
+
+                            client_connection_to_signup.settimeout(None)
+
+                            if signup_message == "FALSE":
+
+                                client_connection_to_signup.close()
+
+                                msgbox.showwarning("Warning","Someone has the username")
+
+                            elif signup_message == "TRUE":   #If no problem for username and password while signing up
+
+                                msgbox.showinfo("Info","Signed up successfully")
+
+                        except socket.timeout:
+
+                            client_connection_to_signup.close()
+
+                            msgbox.showerror("Error","Connection timeout")
+
+                        except:
+
+                            client_connection_to_signup.close()
+
+                            msgbox.showerror("Error","Connection has been shutted")
+
+                        else:
+
+                            pass
+                
+                else:
+
+                    client_connection_to_signup.close()
+
+                    msgbox.showwarning("Warning","Username and Password must be at least 4 characters")
+
+        self.signup_button_object.configure(text="Log in",command=self.__signup_window_widgets_after_click_login_button)
+
+        self.enter_button_object.configure(text="Sign up",command=sign_up_person)
+
+    def __signup_window_widgets_after_click_login_button(self):
+
+        def placeholder_for_username_field(event):
+
+            self.username_entry_object["state"] = "normal"
+
+            self.username_entry_object.delete(0,"end")
+
+            self.username_entry_object.unbind("<Button-1>",self.placeholder_username_id)
+
+        self.username_entry_object["state"] = "normal"
+
+        self.username_entry_object.delete(0,"end")
+
+        self.username_entry_object.insert(0,"Username")
+
+        self.username_entry_object.configure(state="disabled",disabledbackground="white")
+
+        self.placeholder_username_id = self.username_entry_object.bind("<Button-1>",placeholder_for_username_field)
+
+        def placeholder_for_password_field(event):
+
+            self.password_entry_object["state"] = "normal"
+
+            self.password_entry_object.delete(0,"end")
+
+            self.password_entry_object.unbind("<Button-1>",self.placeholder_password_id)
+
+            self.password_entry_object["show"] = "*"
+
+        self.password_entry_object.configure(show="",state="normal")
+
+        self.password_entry_object.delete(0,"end")
+
+        self.password_entry_object.insert(0,"Password")
+
+        self.password_entry_object.configure(state="disabled",disabledbackground="white")
+
+        self.placeholder_password_id = self.password_entry_object.bind("<Button-1>",placeholder_for_password_field)
+
+
+
+
+        self.signup_button_object.configure(text="Sign up",command=self.__after_click_signup_button)
+
+        self.enter_button_object.configure(text="Enter",command=self.__open_the_chat_window)
+    
+        
+            
+class ChatWindow(tk.Toplevel):
+
+    def __init__(self,master_window:tk.Tk):
+
+        super().__init__(master_window)
+
+        self.master_window = master_window
+
+        self.wm_geometry("700x800")
+
+        self.wm_minsize(670,740)
+
+        self.wm_maxsize(1920,1080)
+
+        self.wm_resizable(True,True)
+
+        self.wm_title("Chat")
+
+        self.wm_protocol('WM_DELETE_WINDOW',self.master_window.destroy)
+
+        self.master_window.wm_withdraw()
+
+    def create_input_of_users_and_return_object(self):
+
+        self.user_input_object = scrolledtxt.ScrolledText(
+        self,
+        )
+
+        self.user_input_object.place_configure(relx=0.11,rely=0.8,relheight=0.14,relwidth=0.65)
+
+        return self.user_input_object
+
+    def create_text_widget_to_display_messages_and_return_object(self):
+
+        self.displaying_messages_object = scrolledtxt.ScrolledText(
+        self,
+        state="disabled"
+        )
+
+        self.displaying_messages_object.place_configure(relx=0.11,rely=0.09,relheight=0.68,relwidth=0.65)
+
+        return self.displaying_messages_object
+
+    def create_send_button_and_return_object(self):
+
+        self.send_button_object = tk.Button(
+        self,
+        text="Send",
+        bg="white",
+        fg="black",
+        state="normal",
+        activebackground="white",
+        activeforeground="black",
+        disabledforeground="black",
+        cursor="hand2",
+        font=(100)
+        )
+
+        self.send_button_object.place_configure(relx=0.83,rely=0.85,relheight=0.05,relwidth=0.12)
+
+        return self.send_button_object
+
+    
+        
+
+class SocketConnection(socket.socket):
+
+    def __init__(self,ip_version,socket_type,*server_ip_and_port):
+
+        super().__init__(ip_version,socket_type)
+
+        self.connect(server_ip_and_port)
+
+    def recv_messages(self,show_incoming_messages_on_screen:scrolledtxt.ScrolledText): #Thread will be handling
+
+        self.show_incoming_messages_on_screen = show_incoming_messages_on_screen
+
+        while True:
+
+            try:
+
+                self.incoming_messages = self.recv(1024).decode("utf-8")
+
+            except:
+
+                pass
+
+            else:
+
+                self.show_incoming_messages_on_screen["state"] = "normal"
+
+                self.show_incoming_messages_on_screen.insert(tk.END,self.incoming_messages)
+
+                self.show_incoming_messages_on_screen.see(tk.END)
+
+                self.show_incoming_messages_on_screen["state"] = "disabled"
+
+    def send_messages(self,messages_in_user_input_scrolledtext:scrolledtxt.ScrolledText,username):
+
+        self.messages_in_user_input_scrolledtext = messages_in_user_input_scrolledtext
+
+        self.username = username
+
+        self.messages_to_be_sent = self.messages_in_user_input_scrolledtext.get("1.0",tk.END)
+
+        self.messages_in_user_input_scrolledtext.delete("1.0","end")
+
+        if self.messages_to_be_sent.isspace() == False:
+
+            try:
+
+                self.send(f"{self.username}:{self.messages_to_be_sent}".encode("utf-8"))
+
+            except:
+
+                pass
+
+main_window = MainWindow()
+
+main_window.set_and_place_enter_button()
+
+main_window.set_and_place_password_entry()
+
+main_window.set_and_place_signup_button()
+
+main_window.set_and_place_username_entry()
+
+main_window.set_and_place_show_label()
+
+main_window.set_and_place_info_label()
+
+main_window.mainloop()
---
+++ Chat room/server.py
@@ -0,0 +1,241 @@
+import socket                                                 #SMALL CHAT ROOM
+import sqlite3                                                #Cannot modify data on the database from clients if do not send messages via network
+import threading                                              #because sqlite has no server
+
+#VERSION:3.9.7          #The person number can be increased using the mysql database and also clients can access the mysql database easily without manually sending messages via network
+                        #and listen() parameter can be increased
+class SqliteDatabase:
+
+    __all_online_clients = set()  #This is defined here because it is like a temporary database
+
+    def start_sql_connection_and_set_username_password(self,username,password):
+        """                                         
+        if you want to use sql or modify something on the database,
+        you have to use this method first.Otherwise it raises error. 
+        """
+
+        self.username = username
+
+        self.password = password
+                                                        
+        self.conn = sqlite3.connect("chat_room_app.db")
+
+        self.cursor = self.conn.cursor()
+
+        self.__create_table()
+
+    def __create_table(self):
+
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS Persons(Id INTEGER PRIMARY KEY,Username TEXT UNIQUE,Password TEXT NOT NULL)")
+
+    def add_person_into_database(self):
+
+        self.cursor.execute("SELECT Id FROM Persons")    #for dynamic Id in sql
+
+        self.id_number_list = self.cursor.fetchall()
+
+        self.id_number = len(self.id_number_list)
+
+        self.id_number += 1
+        
+        self.cursor.execute(f"INSERT INTO Persons VALUES({self.id_number},'{self.username}','{self.password}')")
+
+    def has_username(self,control_username):
+
+        self.cursor.execute(f"SELECT Username FROM Persons WHERE Username = '{control_username}'")
+
+        self.result = self.cursor.fetchone()
+
+        if self.result == None:
+
+            return True
+
+        else:
+
+            return False
+
+    def login(self,username_to_login,password_to_login):
+
+        self.cursor.execute(f"SELECT Username,Password FROM Persons WHERE Username = '{username_to_login}' AND Password = '{password_to_login}'")
+
+        self.result = self.cursor.fetchone()
+
+        if self.result == None:
+
+            return False
+
+        else:
+
+            return True
+
+    def close_sql_connection_and_commit(self):
+
+        self.conn.commit()
+
+        self.conn.close()
+
+    @classmethod
+    def online_clients_in_chatroom_for_broadcast_message(cls)-> set[socket.socket]:
+
+        return cls.__all_online_clients
+
+    @classmethod
+    def add_client_that_entered_chatroom_for_broadcast_message(cls,conn):
+
+        cls.__all_online_clients.add(conn)
+
+    @classmethod
+    def delete_client_that_left_chatroom_for_broadcast_message(cls,conn):
+
+        cls.__all_online_clients.remove(conn)
+
+class SocketConnection(socket.socket):
+
+    def __init__(self,ip_version,socket_type,server_ip,server_port):
+
+        super().__init__(ip_version,socket_type)
+
+        self.server_ip = server_ip
+
+        self.server_port = server_port
+
+    def __bind_ip_to_port(self):
+
+        self.bind((self.server_ip,self.server_port))
+
+    def start_connection(self):
+
+        self.__bind_ip_to_port()
+
+        self.listen(15)
+
+    def _wait_for_messages_and_send(self,conn_obj:socket.socket):
+
+        while True:
+
+            self.conn_obj = conn_obj
+
+            try:
+
+                self.message = self.conn_obj.recv(1024)
+
+            except:
+
+                break
+
+            else:
+
+                if self.message.decode("utf-8").startswith("****UsErNamE---PasSwORd__??") == True:
+
+                    self.username_password_list = self.message.decode("utf-8")[28:].split(" ")
+
+                    database_object.start_sql_connection_and_set_username_password(self.username_password_list[0],self.username_password_list[1])
+
+                    self.check_username = database_object.has_username(self.username_password_list[0])
+
+                    if self.check_username == False:
+
+                        try:
+
+                            self.conn_obj.send("FALSE".encode("utf-8"))  #Someone has the username
+
+                        except:
+
+                            pass
+
+                        database_object.close_sql_connection_and_commit()
+
+                    else:
+
+                        try:
+
+                            self.conn_obj.send("TRUE".encode("utf-8"))
+
+                        except:
+
+                            pass
+
+                        lock.acquire()
+                        
+                        database_object.add_person_into_database()
+
+                        database_object.close_sql_connection_and_commit()
+
+                        lock.release()
+
+                elif self.message.decode("utf-8").startswith("****UsErNamEToLOgin---PasSwORdToLOgin__??"):
+
+                    self.username_password_list_to_login = self.message.decode("utf-8")[42:].split(" ")
+
+                    database_object.start_sql_connection_and_set_username_password(self.username_password_list_to_login[0],self.username_password_list_to_login[1])
+
+                    self.check_username_and_password = database_object.login(self.username_password_list_to_login[0],self.username_password_list_to_login[1])
+
+                    if self.check_username_and_password == False:
+
+                        try:
+
+                            self.conn_obj.send("FALSE".encode("utf-8"))  #Wrong username or password
+
+                        except:
+
+                            pass
+
+                        database_object.close_sql_connection_and_commit()
+
+                    else:
+
+                        try:
+
+                            self.conn_obj.send("TRUE".encode("utf-8"))
+
+                        except:
+
+                            pass
+
+                        SqliteDatabase.add_client_that_entered_chatroom_for_broadcast_message(self.conn_obj)
+
+                        database_object.close_sql_connection_and_commit()
+
+                elif self.message.decode("utf-8") == "":     #if any client closes the connection,server side connection that linked the client's connection shuts too.
+
+                    break
+
+                else: #broadcast message
+
+                    for conns in SqliteDatabase.online_clients_in_chatroom_for_broadcast_message():
+
+                                                       #The client sends itself the message too while broadcasting.
+                                                       #In the client side script,if the message does not display on the chat screen,it means the client has network problems.
+                        try:
+
+                            conns.send(self.message)
+
+                        except:
+
+                            pass
+
+        if self.conn_obj in SqliteDatabase.online_clients_in_chatroom_for_broadcast_message():
+
+            SqliteDatabase.delete_client_that_left_chatroom_for_broadcast_message(self.conn_obj)
+
+        self.conn_obj.close()
+
+database_object = SqliteDatabase()
+
+lock = threading.Lock()  #Thread lock mechanism for persons who will be added into the database with Id
+
+chatroom_connection = SocketConnection(socket.AF_INET,socket.SOCK_STREAM,"127.0.0.1",11111)  #İPv4,TCP,LOCAL MACHİNE
+
+chatroom_connection.start_connection()
+
+while True:
+
+    conn,addr = chatroom_connection.accept()
+
+    thread_to_get_message = threading.Thread(target=chatroom_connection._wait_for_messages_and_send,args=(conn,))
+
+    thread_to_get_message.start()
+
+
+            
